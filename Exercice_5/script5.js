b56e5c0d94c7e69d08a6d2bce4afe3b6b5fc324a
Pokmand <manudesj@msn.com>
2019-04-08 14:36:27 +0200

    5et 6 ok

---
+++ Exercice_5/script5.js
@@ -0,0 +1,6 @@
+function changeimage(cascade) {
+document.getElementById(cascade.id).src="images/" + cascade.id + "_2.jpg";
+}
+function changeimage2(cascade) {
+document.getElementById(cascade.id).src="images/" + cascade.id + ".jpg";
+}
